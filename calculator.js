5aa7af33b7e62023fab3ee659fb157b7b1339cf8
Dhrubajyoti-Pramanick <dhrubajyoti@itobuz.com>
2023-01-31 11:01:57 +0530

    2nd commit

--- calculator.js
+++ calculator.js
@@ -1,30 +1,3 @@
-// function disp(value) {
-//     document.getElementById("result").value+= value;
-//     event.preventDefault();
-// }
-// function clearScreen(val) {
-//     document.getElementById(val).innerHTML ="";
-// }
-// function calc() {
-//     let exp=document.display.textview.value;
-//     document.display.textview.value=result(exp);
-// }
-// function result(exp) {
-//     return new Function('return ' +exp)();
-// }
-
-{/* <script>
-        function insert(num){
-            document.form1.textview.value=document.form1.textview.value+num;
-        }
-        function equal(){
-            let exp=document.form1.textview.value;
-            document.form1.textview.value=result(exp);
-        }
-        function result(exp){
-            return new Function('return '+exp)();
-        }
-    </script> */}
 
     function disp(num){
         document.querySelector("#result").value+= num;
@@ -32,14 +5,16 @@
     }
 
     function evaluation(fn) {
-        return new Function ('return '+fn);
+        return new Function ('return '+fn)();
     }
 
-    document.querySelector(".result").addEventListener("click",function calc(){
-        let input = document.querySelector("#result").value;
+    let input;
+    function calc() {
+        event.preventDefault();
+        input = document.querySelector("#result").value;
         let result = evaluation(input);
         document.querySelector("#result").value=result;
-    })
+    }
 
     function clearScreen(val) {
          document.getElementById(val).innerHTML =""; 
